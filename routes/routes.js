0e14278d00c669e79e9cd38b749fa75f9e7d7509
edgareor <edgareor@hotmail.es>
2021-04-10 14:54:33 -0400

    add valids check and others upgrade

--- routes/routes.js
+++ routes/routes.js
@@ -1,31 +1,77 @@
 const { Router } = require('express');
+const { check, validationResult } = require('express-validator');
 
 let router = Router();
 
-router.get('/:paramTest', function (req, res) {
-    console.log(req.params.paramTest);
-    res.json({ message: req.params.paramTest })
-})
+router.get('/:paramTest',
+    [/*Validaciones check*/],
+    (req, res) => {
 
-router.post('/', function (req, res) {
-    console.log(req.body);
-    res.status(201).json({ body: req.body })
-})
+        //Resultados de Validacion
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json(errors);
+        }
 
-router.put('/', function (req, res) {
-    res.status(400).json({
-        error: 'Error',
-        message: 'Put test'
+        console.log(req.params.paramTest);
+        res.status(200).json({ message: req.params.paramTest })
     })
-})
 
-router.delete('/', function (req, res) {
-    console.log(req.query);
-    res.json({ message: req.query })
-})
+router.post('/',
+    [/*Validaciones check*/],
+    (req, res) => {
 
-router.patch('/', function (req, res) {
-    res.json({ message: 'Patch test' })
-})
+        //Resultados de Validacion
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json(errors);
+        }
+
+        console.log(req.body);
+        res.status(201).json({ body: req.body })
+    })
+
+router.put('/',
+    [/*Validaciones check*/],
+    (req, res) => {
+
+        //Resultados de Validacion
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json(errors);
+        }
+
+        res.status(400).json({
+            error: 'Error',
+            message: 'Put test'
+        })
+    })
+
+router.delete('/',
+    [/*Validaciones check*/],
+    (req, res) => {
+
+        //Resultados de Validacion
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json(errors);
+        }
+
+        console.log(req.query);
+        res.status(200).json({ message: req.query })
+    })
+
+router.patch('/',
+    [/*Validaciones check*/],
+    (req, res) => {
+
+        //Resultados de Validacion
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json(errors);
+        }
+
+        res.json({ message: 'Patch test' })
+    })
 
 module.exports = router
