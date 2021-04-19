b7f969b5dff290b9be2b0c4695db6d3b3a115150
edgareor <edgareor@hotmail.es>
2021-04-19 15:29:18 -0400

    mod process.env

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');
 
-const db = new Sequelize('test', 'root', 'admin', {
+const db = new Sequelize(process.env.BD, process.env.USER, process.env.PASSWORD, {
     host: 'localhost',
     dialect: 'mysql',
     logging: true
